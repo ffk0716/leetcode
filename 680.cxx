4684d7789d94f6411540d4376bf5464cbe48b96c
Eric Yu <ffk0716@gmail.com>
2017-09-23 01:56:52 +0800

    new file:   680.cxx

---
+++ 680.cxx
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+class Solution {
+    public:
+        bool check(size_t start, size_t end, const string &s) {
+            while (start <= end) {
+                if (s[start] != s[end])
+                    return false;
+                start++;
+                end--;
+            }
+            return true;
+        }
+
+        bool validPalindrome(const string &s) {
+            size_t start = 0;
+            size_t end = s.size() - 1;
+            while (start <= end) {
+                if (s[start] == s[end]) {
+                    start++;
+                    end--;
+                    continue;
+                }
+                return check(start + 1, end, s) || check(start, end - 1, s);
+            }
+            return true;
+        }
+
+};
+
+int main()
+{
+    Solution  s;
+    cout << s.validPalindrome("abxab") << endl;
+}
