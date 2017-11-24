0554f57637e8198bfab790ee76cdabff25a2cfcf
Eric Yu <ffk0716@gmail.com>
2017-11-24 23:12:30 +0800

    new file:   3.py

---
+++ 3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        cur_str = ""
+        max_len = 0
+        for c in s:
+            i = cur_str.find(c)
+            if i != -1:
+                max_len = max(len(cur_str), max_len)
+                cur_str = cur_str[i + 1:]
+            cur_str += c    
+        return max(len(cur_str), max_len)
+
+
+a = Solution()
+print(a.lengthOfLongestSubstring("abccdjefi"))
+print(a.lengthOfLongestSubstring("abcabcbb"))
+print(a.lengthOfLongestSubstring("dvdf"))
+
