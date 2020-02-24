41f433afc2e341bb3028fb9fc1b071ae47cdfa08
Eric Yu <ffk0716@gmail.com>
2020-02-25 00:46:43 +0800

    修改：     581.shortest-unsorted-continuous-subarray.py

--- 581.shortest-unsorted-continuous-subarray.py
+++ 581.shortest-unsorted-continuous-subarray.py
@@ -10,28 +10,31 @@ class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
         # Time: O(n)
         # Space: O(1)
-        for i in range(1, len(nums)):
-            if nums[i - 1] > nums[i]:
-                min_v = min(nums[i:])
+        #  1 3 8 4 5 6 2 7 9
+        # |  L  |  M  |  R  |
+        for i in range(0, len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                max_L_i = i
                 break
         else:
             return 0
-        for i in range(0, len(nums)):
-            if min_v < nums[i]:
-                subset_start = i
-                break
-
         for i in reversed(range(1, len(nums))):
             if nums[i - 1] > nums[i]:
-                max_v = max(nums[:i])
+                min_R_i = i
                 break
         else:
             assert False
+        max_v = max(nums[max_L_i:min_R_i + 1])
+        min_v = min(nums[max_L_i:min_R_i + 1])
+        for i in range(0, len(nums)):
+            if min_v < nums[i]:
+                min_M_i = i
+                break
         for i in reversed(range(0, len(nums))):
             if max_v > nums[i]:
-                subset_end = i + 1
+                max_M_i = i
                 break
-        return subset_end - subset_start
+        return max_M_i - min_M_i + 1
 
 
 # @lc code=end
